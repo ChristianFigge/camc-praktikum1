88506755f1ca420b0d7431f73f1a866d72251431
ChristianFigge <135016000+ChristianFigge@users.noreply.github.com>
2025-04-21 12:35:07 +0200

    LazyColumn for SensorTypes

--- app/src/main/java/com/example/camc_praktikum1/ui/screens/AllSensorsScreen.kt
+++ app/src/main/java/com/example/camc_praktikum1/ui/screens/AllSensorsScreen.kt
@@ -5,8 +5,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
@@ -39,7 +39,6 @@ fun AllSensorsScreen(
         }
         return false
     }
-
 
     Column() {
         Column(
@@ -78,11 +77,8 @@ fun AllSensorsScreen(
             HorizontalDivider(thickness = 2.dp)
         }
 
-        // SENSOR PANELS
-        Column(
-            modifier = Modifier.verticalScroll(rememberScrollState())
-        ) {
-            SensorTypeData.entries.forEach {
+        LazyColumn() {
+            items(SensorTypeData.entries.toList()) {
                 Spacer(Modifier.height(30.dp))
                 SensorPanel(
                     sensorType = it,
